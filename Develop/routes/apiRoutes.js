740eaed82ff6514ccde6191d36be678292943b9b
Landon Waddell <Landon.waddell@gmail.com>
2021-07-15 10:11:00 -0600

    Added lines to api routes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -5,8 +5,37 @@ const router = express.Router();
 
 let noteData = require("../db/db.json")
 
-    app.get("/api/notes", (req, res)=>{
+    router.get("/api/notes", (req, res)=>{
       return res.json(noteData)
     })
 
 module.exports = router;
+
+
+
+
+// router.get("/api/newNote", function(req, res) {
+//   connection.query("SELECT * FROM notes", function(err, dbnewNote) {
+//     if (err) throw err;
+
+//     res.json(dbnewNote);
+//   });
+// });
+
+// router.post("/api/newNote", function(req, res) {
+//   connection.query("INSERT INTO notes SET ?", [req.body], function(err, result) {
+//     if (err) throw err;
+
+//     res.json(result);
+//   });
+// });
+
+// router.put("/api/newNote/:id", function(req, res) {
+//   connection.query("UPDATE notes SET ? WHERE id = ?", [req.body, req.params.id], function(err, result) {
+//     if (err) throw err;
+
+//     res.json(result);
+//   });
+// });
+
+// module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const router = require('./routes/htmlRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require("./routes/apiRoutes");
 
 const app = express();
-app.use('/', router)
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 const PORT = 8080;
+
+app.use(htmlRoutes);
+app.use(apiRoutes);
 
 app.listen(PORT, () => {
 console.log(`Server listening on: http://localhost:${PORT}`);
